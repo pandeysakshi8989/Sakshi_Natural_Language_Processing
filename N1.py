3a9c9c7aa31871112a0fa5ea523b630b36cc7957
SAKSHI PANDEY <107289486+pandeysakshi8989@users.noreply.github.com>
2026-01-08 10:07:34 +0530

    Add files via upload
    Unit I Study Material + NLP basics + NLP Pipeline

---
+++ N1.py
@@ -0,0 +1,2 @@
+import nltk
+print("NLTK installed successfully!")
---
+++ file.py
@@ -0,0 +1,4 @@
+from Area import calculate
+Radius = int(input("Enter Radius: "))
+area = calculate(Radius)
+print(f"The area is : {area}")
